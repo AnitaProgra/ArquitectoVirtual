7e304f2be32de40f84c8a6a6705580207861933c
VillafaneNicolas <ndvruquet@gmail.com>
2023-10-11 22:31:48 -0300

    Se reemplaza el proyecto base anterior con el proyecto nuevo

---
+++ src/main/java/com/TpTaller/AplicacionArquitecto/AplicacionArquitectoApplication.java
@@ -0,0 +1,13 @@
+package com.TpTaller.AplicacionArquitecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionArquitectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionArquitectoApplication.class, args);
+	}
+
+}
